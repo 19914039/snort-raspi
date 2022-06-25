46793ad90f4a36d7ba7f8f7d91401a14bbd0ec63
Suresh <103111380+19914039@users.noreply.github.com>
2022-06-25 16:43:27 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'snort-raspi'
+copyright = '2021,'
+author = 'Suresh'
 
 release = '0.1'
 version = '0.1.0'
